eb99ef7b646a1844dae1087129c33bafd557ea9d
wpyok500 <wpyok500@gmail.com>
2017-10-31 11:55:05 +0800

    V2ray iptables规则

---
+++ v2ray-iptables.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# Compile:by-wpyok500    2017-10-31
+
+# Create new chain
+iptables -t nat -N V2RAY
+iptables -t mangle -N V2RAY
+iptables -t mangle -N V2RAY_MARK
+
+# Ignore your V2Ray server's addresses
+# It's very IMPORTANT, just be careful.
+iptables -t nat -A V2RAY -d 192.168.123.0/24 -j RETURN
+
+# Ignore LANs and any other addresses you'd like to bypass the proxy
+# See Wikipedia and RFC5735 for full list of reserved networks.
+iptables -t nat -A V2RAY -d 0.0.0.0/8 -j RETURN
+iptables -t nat -A V2RAY -d 10.0.0.0/8 -j RETURN
+iptables -t nat -A V2RAY -d 127.0.0.0/8 -j RETURN
+iptables -t nat -A V2RAY -d 169.254.0.0/16 -j RETURN
+iptables -t nat -A V2RAY -d 172.16.0.0/12 -j RETURN
+iptables -t nat -A V2RAY -d 192.0.0.0/24 -j RETURN
+iptables -t nat -A V2RAY -d 192.0.2.0/24 -j RETURN
+iptables -t nat -A V2RAY -d 192.88.99.0/24 -j RETURN
+iptables -t nat -A V2RAY -d 192.168.0.0/16 -j RETURN
+iptables -t nat -A V2RAY -d 198.51.100.0/24 -j RETURN
+iptables -t nat -A V2RAY -d 1203.0.113.0/24 -j RETURN
+iptables -t nat -A V2RAY -d 224.0.0.0/4 -j RETURN
+iptables -t nat -A V2RAY -d 240.0.0.0/4 -j RETURN
+iptables -t nat -A V2RAY -d 255.255.255.255/32 -j RETURN
+
+# Anything else should be redirected to Dokodemo-door's local port
+iptables -t nat -A V2RAY -p tcp -j REDIRECT --to-ports 1099
+
+# Add any UDP rules
+ip route add local default dev lo table 100
+ip rule add fwmark 1 lookup 100
+iptables -t mangle -A V2RAY -p udp --dport 53 -j TPROXY --on-port 1099 --tproxy-mark 0x01/0x01
+iptables -t mangle -A V2RAY_MARK -p udp --dport 53 -j MARK --set-mark 1
+
+# Apply the rules
+iptables -t nat -A OUTPUT -p tcp -j V2RAY
+iptables -t mangle -A PREROUTING -j V2RAY
+iptables -t mangle -A OUTPUT -j V2RAY_MARK
+
+#save iptables
+service iptables save
+#iptables-save > /etc/sysconfig/iptables
